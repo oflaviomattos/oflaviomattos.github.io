41c14182d34491105856def882bb1211a39b808a
Flavio Mattos <contact@flaviomattos.com>
2023-12-24 13:23:26 -0300

    atualização do index.html CSS e js

---
+++ main.js
@@ -0,0 +1,6 @@
+// You can add custom scripts here if needed
+// For now, let's keep it simple
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Your initialization code here, if any
+});
