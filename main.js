8b5ddeba5afc8118129d08abc0f8d07d9ccf9016
Flavio Mattos <68172848+oflaviomattos@users.noreply.github.com>
2026-03-30 06:40:29 -0300

    Delete main.js

--- main.js
+++
@@ -1,5 +0,0 @@
-var app = angular.module('portfolioApp', []);
-
-app.controller('MainController', function ($scope) {
-    // Se precisar de lógica adicional do Angular, você pode adicionar aqui
-});
